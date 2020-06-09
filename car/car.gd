a7c01b80e8266d44c5e981ad1d823abec211ea8a
thederek <mail@derek.tech>
2020-06-09 23:27:08 +0100

    Made camera follow car and added background

--- car/car.gd
+++ car/car.gd
@@ -1,8 +1,12 @@
 extends RigidBody2D
 
-var driver: KinematicBody2D = null
 export var speed: float = 5000
 export var reverse_speed: float = 2000
+export var drive_zoom = 1.5
+
+var default_zoom = null
+var camera: Camera2D = null
+var driver: KinematicBody2D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,7 +19,6 @@ func _ready():
 
 func get_direction() -> Vector2:
 	var direction = Vector2(0, -1).rotated(rotation)
-	
 	return direction.normalized();
 
 func get_steering() -> float:
@@ -28,16 +31,26 @@ func get_steering() -> float:
 		
 	return direction
 
+func remove_driver():
+	print("Leaving the car")
+	get_parent().add_child(self.driver)
+	self.driver.global_position = $DoorPos.global_position
+
+	if (self.camera):
+		remove_child(self.camera)
+		driver.add_child(self.camera)
+		self.camera.zoom = self.default_zoom
+		self.camera = null
+	
+	self.driver = null
+
 func _physics_process(delta):
 	if !self.driver:
 		return
 	
 	# Remove the driver from the car if they want to leave
 	if Input.is_action_just_pressed("enter_vehicle"):
-		print("Leaving the car")
-		get_parent().add_child(self.driver)
-		self.driver.global_position = $DoorPos.global_position
-		self.driver = null
+		remove_driver()
 	
 	if !Input.is_action_pressed("up"):
 		return
@@ -54,9 +67,17 @@ func _physics_process(delta):
 func add_driver(new_driver: KinematicBody2D):
 	if driver:
 		return false
-	else:
-		driver = new_driver
-		new_driver.get_parent().remove_child(new_driver)
+	
+	driver = new_driver
+	new_driver.get_parent().remove_child(new_driver)
+	self.camera = driver.get_node_or_null("Camera2D")
+	if self.camera:
+		driver.remove_child(self.camera)
+		add_child(self.camera)
+		self.default_zoom = self.camera.zoom
+		self.camera.zoom = Vector2(self.drive_zoom, self.drive_zoom)
+	return true
+
 
 func _on_DetectionArea_body_entered(body):
 	if body.has_method("add_nearby_vehicle"):
--- player/player.gd
+++ player/player.gd
@@ -5,16 +5,13 @@ export var speed: float = 200
 
 var nearby_vehicle = null;
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func _process(delta):
 	if Input.is_action_just_pressed("enter_vehicle"):
 		print(nearby_vehicle)
 		if (nearby_vehicle):
 			print("Entering vehicle")
 			self.nearby_vehicle.add_driver(self)
+
 
 func get_direction() -> Vector2:
 	var direction = Vector2()
@@ -38,14 +35,11 @@ func _physics_process(delta):
 	if direction.length() != 0:
 		move_and_slide(direction * speed, Vector2(0, 0), false, 4, 0.785398, false)
 
-		
 	look_at(get_global_mouse_position())
 
 func add_nearby_vehicle(vehicle: RigidBody2D):
-	print("Nearby vehicle")
 	self.nearby_vehicle = vehicle
 
 func remove_nearby_vehicle(vehicle: RigidBody2D):
 	if (self.nearby_vehicle == vehicle):
-		print("Not nearby")
 		self.nearby_vehicle = null
